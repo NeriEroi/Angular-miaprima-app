d9015fd7450ce6e99fd69f2836fcb4d6a99a5af4
Michele <micheleierniero@gmail.com>
2021-12-26 20:18:24 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SecondoComponent } from './secondo-component/secondo-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'route',
+    component: SecondoComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
