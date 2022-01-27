95d38c375df8a81387c706be262f1703c8c62723
Michele <micheleierniero@gmail.com>
2022-01-27 15:31:35 +0100

    route terzo component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SecondoComponent } from './secondo-component/secondo-component.component';
+import { TerzoComponent } from './terzo-component/terzo-component.component';
 
 const routes: Routes = [
   {
-    path: 'route',
+    path: 'secondo-component',
     component: SecondoComponent,
+  },
+  {
+    path: 'terzo-component',
+    component: TerzoComponent,
   }
 ];
 
